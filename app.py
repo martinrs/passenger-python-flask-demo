ecf1643cc82955815b11dee041fd7d397a385c59
Andersraae <andersraae01@gmail.com>
2021-11-02 14:47:26 +0100

    Undskyldninger

--- app.py
+++ app.py
@@ -3,17 +3,17 @@ from random import choice
 DailyExcuse = Flask(__name__)
 
 excuses = []
-with open('undskyldninger.txt') as f:
+with open('undskyldninger.txt', encoding="utf-8") as f:
 	excuses = f.readlines()
 
 def getRandomExcuse():
 	exc = choice(excuses)
 	if len(exc) != 0:
-		exc = exc[0].lower() + exc[1:len(exc)-1]
+		exc = exc[0].lower() + exc[1:len(exc)]
 		return exc.strip()
 	return None
 
-@DailyExcuse.route("/")
+@DailyExcuse.route("/", methods=['GET'])
 def hello():
 	return render_template('index.html', excuse=getRandomExcuse())
 
